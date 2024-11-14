1970b0c5b39aff05a4b24dabedd562978f1cb1e0
Isaacoun100 <Isaacoun100@estudiantec.cr>
2024-11-14 03:16:42 -0600

    Board game update
    The values on the cells now uses the templates in the boards.json file, meaning the boards now display as they should as if you were to play

--- Futoshiki/src/view/Futoshiki.java
+++ Futoshiki/src/view/Futoshiki.java
@@ -1,18 +1,25 @@
 package view;
 
+import controller.ManageBoard;
+import model.Board;
+import model.Constrain;
 import model.Settings;
-
+import model.Value;
+import org.json.simple.parser.ParseException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Futoshiki extends JPanel implements ActionListener {
 
-    private ArrayList<Button> gameButtons;
+    private ArrayList<JButton> gameButtons;
     private ArrayList<JLabel> contrainsLabel;
     private ArrayList<Button> sideButtons;
+    private String currentPress = "_";
+    private Board board;
 
     public Futoshiki(Settings settings) {
 
@@ -21,16 +28,23 @@ public class Futoshiki extends JPanel implements ActionListener {
         gameButtons = new ArrayList<>();
         sideButtons = new ArrayList<>();
 
+        // This call updates the board that will be used to set the defaults
+        defaultBoard(settings);
+
         JPanel gamePanel = new JPanel(new GridLayout( 2*matrix-1, 2*matrix-1, 10, 10 ));
-        JPanel sidePanel = new JPanel(new GridLayout( matrix, 0, 10, 10 ));
+        JPanel sidePanel = new JPanel(new GridLayout( matrix+1, 0, 10, 10 ));
         JPanel panel = new JPanel(new BorderLayout());
 
         JPanel emptyPanel = new JPanel();
         emptyPanel.setPreferredSize(new Dimension(100, 0));
 
         int currentPosition = 0;
+        int cell_i = 0;
 
         for (int i = 0; i < 2*matrix-1 ; i++) {
+
+            int cell_j = 0;
+            int cons_j = 0;
 
             for (int j = 0; j < 2*matrix-1 ; j++) {
 
@@ -42,30 +56,52 @@ public class Futoshiki extends JPanel implements ActionListener {
                 }
                 else{
                     if(currentPosition%2!=0){
-                        JLabel label = new JLabel(String.valueOf(currentPosition));
+                        JLabel label = new JLabel() ;
+
+                        for (Constrain constraint : board.getConstraints()) {
+
+                            if( constraint.getY1() == constraint.getY2() && i%2==0 && constraint.getY1() == cell_i ){
+                                if( constraint.getX1() == cell_j-1){
+                                    label.setText(constraint.getEquals());
+                                    board.getConstraints().remove(constraint);
+                                    break;
+                                }
+                            }
+                            else if (constraint.getX1() == constraint.getX2() && constraint.getX1() == cons_j ) {
+                                if( constraint.getY1() == cell_i-1){
+                                    label.setText(constraint.getEquals());
+                                    board.getConstraints().remove(constraint);
+                                    break;
+                                }
+                            }
+
+                        }
+
                         label.setPreferredSize( new Dimension( 300/matrix, 300/matrix ) );
                         label.setHorizontalAlignment(SwingConstants.CENTER);
                         gamePanel.add( label );
                         currentPosition++;
+                        cons_j++;
                     }
                     else{
-                        Button cell = new Button(String.valueOf(currentPosition));
-                        cell.setPreferredSize( new Dimension( 300/matrix, 300/matrix ) );
+                        JButton cell = getButton(cell_i, cell_j, matrix);
                         cell.addActionListener(this);
                         gamePanel.add( cell );
                         gameButtons.add( cell );
                         currentPosition++;
+                        cell_j++;
                     }
                 }
             }
+
+            if(i%2==0)
+                cell_i++;
         }
 
         for (int i = 0; i < matrix; i++) {
-            Button option = new Button( String.valueOf( i+1 ) );
-            option.setPreferredSize( new Dimension( 350/matrix, 350/matrix ) );
-            sidePanel.add( option );
-            sideButtons.add( option );
+            addCell(String.valueOf(i), matrix, sidePanel);
         }
+        addCell("Delete", matrix, sidePanel);
 
         panel.add(gamePanel, BorderLayout.WEST);
         panel.add(emptyPanel, BorderLayout.CENTER);
@@ -74,22 +110,43 @@ public class Futoshiki extends JPanel implements ActionListener {
 
     }
 
+    private JButton getButton(int cell_i, int cell_j, int matrix) {
+        JButton cell = new JButton();
+
+        for (Value value : board.getValues()) {
+            if( value.getY()== cell_i && value.getX()== cell_j){
+                cell.setLabel(String.valueOf(value.getValue()));
+                break;
+            }
+        }
+
+        cell.setPreferredSize( new Dimension( 300/ matrix, 300/ matrix) );
+        cell.setBackground(Color.decode("#ADD8E6"));
+        cell.setForeground(Color.black);
+        return cell;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        Button clickedButton = (Button) e.getSource();
+        JButton clickedButton = (JButton) e.getSource();
         System.out.println("Button " + clickedButton.getLabel() + " clicked");
     }
 
-    public ArrayList<Button> getGameButtons() {
-        return gameButtons;
+    private void defaultBoard(Settings settings){
+        try {
+            this.board = ManageBoard.getBoard(settings);
+        }
+        catch (IOException | ParseException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Unable to load board", JOptionPane.ERROR_MESSAGE);
+            throw new RuntimeException(e);
+        }
     }
 
-    public ArrayList<Button> getSideButtons() {
-        return sideButtons;
-    }
-
-    public ArrayList<JLabel> getContrainsLabel() {
-        return contrainsLabel;
+    private void addCell (String label, int matrix, JPanel sidePanel){
+        Button option = new Button(label);
+        option.setPreferredSize( new Dimension( 350/matrix, 350/matrix ) );
+        sidePanel.add( option );
+        sideButtons.add( option );
     }
 
 }
